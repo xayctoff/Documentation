dbe52fa04c34118c3041dd921efdbd38b5c3ecb3
xayctoff <petro310897@gmail.com>
2019-05-12 17:39:50 +0700

    Total refactoring

--- src/main/java/model/Total.java
+++ src/main/java/model/Total.java
@@ -4,23 +4,47 @@ import java.util.ArrayList;
 
 public class Total {
 
-    private ArrayList <Double> sum;
+    private static final int sumCapacity = 5;
 
-    private ArrayList <Double> sideSum;
+    private static ArrayList <Double> sum;
+    private static ArrayList <Double> sideSum;
+
+    static {
+        sideSum = new ArrayList <>(sumCapacity) {
+            {
+                for (int i = 0; i < sumCapacity; i++) {
+                    add(0.0);
+                }
+            }
+        };
+
+        sum = sideSum;
+    }
 
     public ArrayList <Double> getSum() {
         return sum;
     }
 
-    public void setSum(ArrayList <Double> sum) {
-        this.sum = sum;
+    public double getOneSum(int index) {
+        return sum.get(index);
+    }
+
+    public void setOneSum(double value, int index) {
+        sum.set(index, value);
+    }
+
+    public void setSum(double value, Product product) {
+        for (Double item : sum) {
+            int index = sum.indexOf(item);
+            sum.set(index, getOneSum(index) + value * product.getRemainsCount(index));
+        }
     }
 
     public ArrayList <Double> getSideSum() {
         return sideSum;
     }
 
-    public void setSideSum(ArrayList <Double> sideSum) {
-        this.sideSum = sideSum;
+    public void setSideSum(double value, int index) {
+        sideSum.set(index, value);
     }
 }
