722e9da8428c6d1eccbdfe07983db14cce8f97af
xayctoff <petro310897@gmail.com>
2019-05-14 10:07:19 +0700

    Moving getCodes method from Product

--- src/main/java/data/Data.java
+++ src/main/java/data/Data.java
@@ -2,33 +2,54 @@ package data;
 
 import javafx.util.Pair;
 import model.Document;
+import model.Product;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Data {
 
+    private static final String productsFile = "/data/products.txt";
+    private static final String measuresFile = "/data/measures.txt";
     private static final String headerFile = "/data/header.txt";
     private static final String organizationsFile = "/data/organizations.txt";
     private static final String unitsFile = "/data/units.txt";
     private static final String postsFile = "/data/posts.txt";
 
-    private ArrayList <String> getArrayList(String path) throws IOException {
-        FileInputStream stream = new FileInputStream(path);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
-        ArrayList <String> arrayList = new ArrayList <>();
-        String line;
+    public Pair <HashMap <String, String>, HashMap <String, String>> getCodes() {
+        String[] position;
+        HashMap <String, String> productCodes = new HashMap<>();
+        HashMap <String, String> measuresCodes = new HashMap<>();
 
-        while ((line = bufferedReader.readLine()) != null) {
-            arrayList.add(line);
+        try {
+            FileInputStream stream = new FileInputStream(Product.class.getResource(productsFile).getPath());
+            BufferedReader bufferedReader = new BufferedReader
+                    (new InputStreamReader(stream));
+            String line;
+
+            while((line = bufferedReader.readLine()) != null ) {
+                position = line.split("#");
+                productCodes.put(position[0], position[1]);
+            }
+
+            stream = new FileInputStream(Product.class.getResource(measuresFile).getPath());
+            bufferedReader = new BufferedReader(new InputStreamReader(stream));
+
+            while ((line = bufferedReader.readLine()) != null) {
+                position = line.split("#");
+                measuresCodes.put(position[0], position[1]);
+            }
         }
 
-        bufferedReader.close();
+        catch (IOException exception) {
+            exception.printStackTrace();
+        }
 
-        return arrayList;
+        return new Pair<> (productCodes, measuresCodes);
     }
 
     public Pair <ArrayList <String>, ArrayList <String>> getHeader(Document document) {
@@ -66,6 +87,21 @@ public class Data {
         }
 
         return posts;
+    }
+
+    private ArrayList <String> getArrayList(String path) throws IOException {
+        FileInputStream stream = new FileInputStream(path);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+        ArrayList <String> arrayList = new ArrayList <>();
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            arrayList.add(line);
+        }
+
+        bufferedReader.close();
+
+        return arrayList;
     }
 
 }
