36e378a82f18dc0da8ca0ef9ab2c67db1552a69e
xayctoff <petro310897@gmail.com>
2019-05-14 09:57:49 +0700

    Implemented table writing

--- src/main/java/data/Excel.java
+++ src/main/java/data/Excel.java
@@ -1,6 +1,8 @@
 package data;
 
+import javafx.util.Pair;
 import model.Document;
+import model.Product;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -9,6 +11,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Map;
 
 public class Excel {
 
@@ -43,17 +46,17 @@ public class Excel {
             }
 
             if (this.document.getOCPO() != null) {
-                cell = sheet.getRow(4).getCell(73);
+                cell = sheet.getRow(5).getCell(73);
                 cell.setCellValue(this.document.getOCPO());
             }
 
             if (this.document.getOrganization() != null) {
-                cell = sheet.getRow(5).getCell(0);
+                cell = sheet.getRow(5).getCell(24);
                 cell.setCellValue(this.document.getOrganization());
             }
 
             if (this.document.getUnit() != null) {
-                cell = sheet.getRow(7).getCell(0);
+                cell = sheet.getRow(7).getCell(24);
                 cell.setCellValue(this.document.getUnit());
             }
 
@@ -85,6 +88,122 @@ public class Excel {
             if (this.document.getResponsibleFace() != null) {
                 cell = sheet.getRow(15).getCell(33);
                 cell.setCellValue(this.document.getResponsibleFace());
+            }
+
+            if (this.document.getCheckingPost() != null) {
+                cell = sheet.getRow(68).getCell(34);
+                cell.setCellValue(this.document.getCheckingPost());
+            }
+
+            if (this.document.getCheckingFace() != null) {
+                cell = sheet.getRow(68).getCell(66);
+                cell.setCellValue(this.document.getCheckingFace());
+            }
+
+            if (this.document.getProducts() != null) {
+
+                int row = 24;
+
+                for (Product product : document.getProducts()) {
+
+                    int column = 0;
+
+                    if (product.getPosition() != null) {
+
+                        if (product.getPosition() > Document.maxFrontSideRows) {
+                            column = 34;
+
+                            for (Double sum : document.getTotal().getSideSum()) {
+
+                                if (sum != null) {
+                                    cell = sheet.getRow(row).getCell(column += 10);
+                                    cell.setCellValue(sum);
+                                }
+
+                            }
+
+                            row = 43;
+                            column = 0;
+                        }
+
+                        cell = sheet.getRow(row).getCell(column += 3);
+                        cell.setCellValue(product.getPosition());
+                    }
+
+                    if (product.getTitle() != null) {
+                        cell = sheet.getRow(row).getCell(column += 12);
+                        cell.setCellValue(product.getTitle());
+                    }
+
+                    if (product.getCode() != null) {
+                        cell = sheet.getRow(row).getCell(column += 4);
+                        cell.setCellValue(product.getCode());
+                    }
+
+                    if (product.getMeasures() != null) {
+                        cell = sheet.getRow(row).getCell(column += 4);
+                        cell.setCellValue(product.getMeasures());
+                    }
+
+                    if (product.getOKEI() != null) {
+                        cell = sheet.getRow(row).getCell(column += 4);
+                        cell.setCellValue(product.getOKEI());
+                    }
+
+                    if (product.getCost() != null) {
+                        cell = sheet.getRow(row).getCell(column);
+                        cell.setCellValue(product.getCost());
+                    }
+
+                    column = 30;
+                    int headerRow = 19;
+                    int headerColumn = 31;
+
+                    for (Map.Entry <String, Pair <Integer, Double>> remain : product.getRemains().entrySet()) {
+
+                        if (remain != null) {
+                            String date = remain.getKey();
+                            int count = remain.getValue().getKey();
+                            double cost = remain.getValue().getValue();
+
+                            cell = sheet.getRow(headerRow).getCell(headerColumn += 2);
+                            cell.setCellValue(date.substring(0, 2));
+
+                            cell = sheet.getRow(headerRow).getCell(headerColumn += 4);
+                            cell.setCellValue(date.substring(3, 5));
+
+                            cell = sheet.getRow(headerRow).getCell(headerColumn += 4);
+                            cell.setCellValue(date.substring(6));
+
+                            cell = sheet.getRow(row).getCell(column += 4);
+                            cell.setCellValue(count);
+
+                            cell = sheet.getRow(row).getCell(column += 6);
+                            cell.setCellValue(cost);
+                        }
+
+                    }
+
+                    row++;
+
+                }
+
+            }
+
+            int row = 65;
+            int column = 34;
+            int index = 0;
+
+            for (Double sum : document.getTotal().getSum()) {
+
+                if (sum != null) {
+                    cell = sheet.getRow(row).getCell(column);
+                    cell.setCellValue(sum - document.getTotal().getOneSideSum(index++));
+
+                    cell = sheet.getRow(row + 1).getCell(column += 10);
+                    cell.setCellValue(sum);
+                }
+
             }
 
             stream.close();
